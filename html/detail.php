dbccc2f5eb154b2032c233fcebda21ccffc3ba89
mocoma <mocomaminecraft@gmail.com>
2022-05-29 13:45:11 +0200

    Changed name to GeImagen to be spanish

---
+++ html/detail.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="application-name" content="geimagen" />
+		<meta name="author" content="Mario Lombas - UO275901" />
+
+		<link rel="stylesheet" href="../css/estilo-pc.css" />
+		<link rel="stylesheet" href="../css/estilo-movil.css" />
+
+		<title>GeImagen - Detalle</title>
+	</head>
+</html>
