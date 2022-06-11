d19feedf55eb6e586059902454630654248b8695
mocoma <mocomaminecraft@gmail.com>
2022-06-12 01:40:24 +0200

    Added spacing between forms on mobile

--- html/add.php
+++ html/add.php
@@ -4,6 +4,7 @@
 		<meta charset="UTF-8" />
 		<meta name="application-name" content="geimagen" />
 		<meta name="author" content="Mario Lombas - UO275901" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
